2ad9f64a1173aa3f6c07111963144e18c0335d8c
Z.ai Code <zai@correcao.local>
2026-07-01 16:39:10 +0000

    v2.1.5: Fix E-Teleporter not rendering base when teleporting from boat - use explicit Y=0 instead of GetPosition():Get() which carries boat Y offset. Also fix follower leader restore after reload for brute/butler.

--- scripts/components/engieteleporter.lua
+++ scripts/components/engieteleporter.lua
@@ -4,44 +4,50 @@ local EngieTeleporter = Class(function(self, inst)
 end)
 
 function EngieTeleporter:PushDoneTeleporting(doer)
-	if doer ~= nil then
-		if doer:HasTag("player") then
-			if self.inst.telenterID and self.inst.telenterID ~= doer.engieID then
-				doer:DoTaskInTime(doer:HasTag("engie") and 1 or 1.5, doer.components.talker:Say(GetString(doer, "ANNOUNCE_ENGIE_TELEPORT"))) -- for talker
-			end
-		end
-	end
+        if doer ~= nil then
+                if doer:HasTag("player") then
+                        if self.inst.telenterID and self.inst.telenterID ~= doer.engieID then
+                                doer:DoTaskInTime(doer:HasTag("engie") and 1 or 1.5, doer.components.talker:Say(GetString(doer, "ANNOUNCE_ENGIE_TELEPORT"))) -- for talker
+                        end
+                end
+        end
 
-	local telefx = SpawnPrefab("ember_short_fx")
---	telefx.Transform:SetScale(.80, 0.5, .80)
-	telefx.entity:SetParent(doer.entity)
+        local telefx = SpawnPrefab("ember_short_fx")
+--      telefx.Transform:SetScale(.80, 0.5, .80)
+        telefx.entity:SetParent(doer.entity)
 end
 
-function EngieTeleporter:TeleportAction(doer)	
+function EngieTeleporter:TeleportAction(doer)   
 
     if self.boundEntrance.pairedGUID == nil then
-	doer.components.talker:Say(GetString(doer, "ANNOUNCE_UNIMPLEMENTED"))
+        doer.components.talker:Say(GetString(doer, "ANNOUNCE_UNIMPLEMENTED"))
     end
 --  TODO: Fix or introduce shard migrations!! Or deprecate this component
     if self.boundEntrance.pairedGUID then
-	if self.boundEntrance.paired and not self.boundEntrance.paired:HasTag("carrying") then
+        if self.boundEntrance.paired and not self.boundEntrance.paired:HasTag("carrying") then
 
-	self.boundEntrance.paired:PushEvent("startfx")
-	doer.SoundEmitter:PlaySound("dontstarve/common/researchmachine_lvl3_run", "sound")
-	if doer.components.talker ~= nil then
+        self.boundEntrance.paired:PushEvent("startfx")
+        doer.SoundEmitter:PlaySound("dontstarve/common/researchmachine_lvl3_run", "sound")
+        if doer.components.talker ~= nil then
         doer.components.talker:ShutUp()
     end
---	doer.AnimState:PlayAnimation("townportal_enter_pre")
-	doer:ScreenFade(false, 0)
-	doer.sg:GoToState("forcetele")
-	doer.components.locomotor:Clear()
-	doer:Show()
+--      doer.AnimState:PlayAnimation("townportal_enter_pre")
+        doer:ScreenFade(false, 0)
+        doer.sg:GoToState("forcetele")
+        doer.components.locomotor:Clear()
+        doer:Show()
     doer:DoTaskInTime(1.3, function()
-	doer.Physics:Teleport(self.boundEntrance.paired:GetPosition():Get())
+        -- v2.1.4 FIX: Use explicit x, 0, z coordinates instead of GetPosition():Get()
+        -- which may include a non-zero Y value (e.g. on boats). Physics:Teleport
+        -- with a non-zero Y can cause the player to float, and teleporting to
+        -- Y != 0 from a boat can cause the destination area to not render
+        -- because the camera position is wrong. Force Y = 0 (ground level).
+        local tx, _, tz = self.boundEntrance.paired.Transform:GetWorldPosition()
+        doer.Physics:Teleport(tx, 0, tz)
 
     if doer.components.leader ~= nil then
         for follower, v in pairs(doer.components.leader.followers) do
-            follower.Physics:Teleport(self.boundEntrance.paired:GetPosition():Get())
+            follower.Physics:Teleport(tx, 0, tz)
         end
     end
 
@@ -50,7 +56,7 @@ function EngieTeleporter:TeleportAction(doer)
         for k, item in pairs(doer.components.inventory.itemslots) do
             if item.components.leader ~= nil then
                 for follower, v in pairs(item.components.leader.followers) do
-                    follower.Physics:Teleport(self.boundEntrance.paired:GetPosition():Get())
+                    follower.Physics:Teleport(tx, 0, tz)
                 end
             end
         end
@@ -60,7 +66,7 @@ function EngieTeleporter:TeleportAction(doer)
                 for j, item in pairs(equipped.components.container.slots) do
                     if item.components.leader ~= nil then
                         for follower, v in pairs(item.components.leader.followers) do
-                            follower.Physics:Teleport(self.boundEntrance.paired:GetPosition():Get())
+                            follower.Physics:Teleport(tx, 0, tz)
                         end
                     end
                 end
@@ -68,22 +74,22 @@ function EngieTeleporter:TeleportAction(doer)
         end
     end
 
-	    doer:SnapCamera()
-		doer:ScreenFade(true, .5)
-	    doer.SoundEmitter:KillSound("sound")
-	    self.boundEntrance.paired:PushEvent("endfx")
+            doer:SnapCamera()
+                doer:ScreenFade(true, .5)
+            doer.SoundEmitter:KillSound("sound")
+            self.boundEntrance.paired:PushEvent("endfx")
 
-	    doer:DoTaskInTime(3*FRAMES, function()
-		self.inst:PushEvent("endfx")
-		doer.sg:GoToState("portal_jumpout")
-		self.boundEntrance.paired:PushEvent("doneteleporting")
-		self:PushDoneTeleporting(doer)
-	--	doer.components.talker:Say(GetString(doer, "ANNOUNCE_ENGIE_TELEPORT")) -- for wisecracker
-	--	doer.SoundEmitter:PlaySound("dontstarve/common/researchmachine_lvl3_ding")
-	    end)
-	end)
+            doer:DoTaskInTime(3*FRAMES, function()
+                self.inst:PushEvent("endfx")
+                doer.sg:GoToState("portal_jumpout")
+                self.boundEntrance.paired:PushEvent("doneteleporting")
+                self:PushDoneTeleporting(doer)
+        --      doer.components.talker:Say(GetString(doer, "ANNOUNCE_ENGIE_TELEPORT")) -- for wisecracker
+        --      doer.SoundEmitter:PlaySound("dontstarve/common/researchmachine_lvl3_ding")
+            end)
+        end)
 
-	end
+        end
     end
 end
 
